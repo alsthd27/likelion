5e7ad3f5a07fcb383d0ab73e4539582b55657a53
Yomin Song <alsthd27@naver.com>
2020-10-07 23:29:43 +0900

    app_name

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog' # app_name 설정 시 템플릿 태그에도 입력해야 함.
 urlpatterns = [
     path('', views.main),
-    path('1/', views.one, name="one"),
+    path('1/', views.one, name="one"), # urls.py에서 name 설정하면 템플릿 태그 활용 가능.
     path('2/', views.two, name="two"),
     path('3/', views.three, name="three"),
 ]
