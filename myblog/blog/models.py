6c3fdd31c92ddf4f885fd510390425707d99ba8e
Yomin Song <alsthd27@naver.com>
2020-10-08 02:17:12 +0900

    models.py Post

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Django에서 model은 class 개념, models의 Model을 상속받는다!!!!!
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, null=False) # 짧은 문자열 필드
+    content = models.TextField() # 긴 문자열 필드
+    view_count = models.IntegerField(default=0) # 정수형 타입
+    created_at = models.DateTimeField(auto_now_add=True) # 날짜 및 시간 타입
+    updated_at = models.DateTimeField(auto_now=True)
