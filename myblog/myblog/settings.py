2a82229976fd26e5bd1a9439b7535a36fd8f915b
Yomin Song <alsthd27@naver.com>
2020-10-07 22:32:35 +0900

    app 생성: blog

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -31,11 +31,11 @@ SECRET_KEY = ')2&2br*9w*zpi#iy%-4l0-^b$fh1b&hvm2_y9+($e_r%2ss-d6'
 DEBUG = True
 
 # 프로젝트 접근가능 호스트 설정. 도메인을 string으로 삽입. 지금은 일단 모두 허용하기 위해 *로 설정.
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
-# 앱 생성할 때마다 여따 추가해야 된다구~~~~~~~~~~
+# 앱 생성할 때마다 여따 추가해야 된다구~~~~~~~~~~ 맨 뒤에 ,까지 쓰는 거 잊지 말라구~~~~~~~~~~~~
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 # 뭔지 잘 몰겄지만 보안 관련 프레임워크~
