7dd3c70a4fb564d4778b95890b29cefe44bef8e4
Yomin Song <alsthd27@naver.com>
2020-10-13 03:37:37 +0900

    필요 없는 html 삭제 & create 함수 views.py, urls.py

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import main, one, two, three, new
+from .views import main, new, create
 
-app_name = 'blog' # app_name 설정 시 템플릿 태그에도 입력해야 함.
+app_name = 'blog' # app_name 설정하는 이유: 서로 다른 앱에서 동일한 이름의 템플릿 파일이 생성될 수도 있으니 app_name으로 구분하는 것.
 urlpatterns = [
-    path('', main, name="main"),
-    path('1/', one, name="one"), # urls.py에서 name 설정하면 템플릿 태그 활용 가능.
-    path('2/', two, name="two"),
-    path('3/', three, name="three"),
+    path('', main, name="main"), # urls.py에서 name 설정하면 템플릿 태그 활용 가능.
     path('new/', new, name="new"),
+    path('create/', create, name="create"),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,23 +1,22 @@
-from django.shortcuts import render # "django.shortcuts 패키지에 있는 render 함수를 사용하로독 하겄따잉!"
-
-
-def main(request): # "이걸 main이라는 함수로 명명하겠다! request를 인자로 받는다!"
-    return render(request, 'blog/main.html') # "render 함수를 활용해 request를 blog/main.html으로 return하겠다!"
+# render는 '템플릿을 불러오는 것', redirect는 'URL로 이동하는 것(해당 URL의 views 함수를 실행하는 것)'.
+from django.shortcuts import render, redirect # "django.shortcuts 패키지에 있는 render 함수 등을 사용하로독 하겄따잉!"
+from .models import Post
 
 # 함수와 함수 사이는 두 줄 간격이 좋다!
 
-
-def one(request):
-    return render(request, 'blog/1.html')
-
-
-def two(request):
-    return render(request, 'blog/2.html')
-
-
-def three(request):
-    return render(request, 'blog/3.html')
+def main(request): # "이걸 main이라는 함수로 명명하겠다! request를 인자로 받는다!"
+    all_posts = Post.objects.all() # "Post 클래스의 모든 항목들을 all_posts(변수)라고 부르겠다!"
+    # Django에서 render의 세 번째 인자는 Python의 딕셔너리 타입 값을 넣어줘야 함.
+    return render(request, 'blog/main.html', {'posts': all_posts}) # "request의 변수(all_posts)를 posts라는 이름으로 명명하고, 해당 request를 render 함수를 활용해 blog/main.html으로 return하겠다!"
 
 
 def new(request):
     return render(request, 'blog/new.html')
+
+
+def create(request):
+    if request.method == "POST":
+        post_title = request.POST.get('title')
+        post_content = request.POST.get('content')
+        Post.objects.create(title=post_title, content=post_content)
+    return redirect('blog:main') # blog:main이라는 URL로 이동 -> blog.views에서 main 함수 실행 -> blog/main.html을 render
