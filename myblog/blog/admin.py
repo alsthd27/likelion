23766e69d996873253d40720a9d7e5ad7c40c91f
Yomin Song <alsthd27@naver.com>
2020-10-15 00:34:08 +0900

    Like 모델링

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-@admin.register(Post) # @는 함수나 클래스를 꾸며주는 데코레이터!!!
+@admin.register(Post) # @는 함수나 클래스를 꾸며주는 데코레이터!!! 새로운 함수를 만들 수 있다!! (중복을 줄여서 코드를 간결하게 만들 수 있다.)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'id',
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -18,6 +18,22 @@ class Post(models.Model):
     view_count = models.IntegerField(default=0) # 정수형 타입
     created_at = models.DateTimeField(auto_now_add=True) # 날짜 및 시간 타입
     updated_at = models.DateTimeField(auto_now=True)
+    '''
+    N:M 관계이므로 ManyToManyField 활용. User(M) : Post(N)
+    게시글에 좋아요가 하나도 없을 수 있으니 blank=True
+    through는 어느 모델을 통해 관계가 이루어지는지(정의되는지) 설정하는 것. Like 모델을 통해 User와 Post의 좋아요(M:N) 관계가 정의되고 있다.
+    '''
+    like_user_set = models.ManyToManyField(User, blank=True, related_name="like_user_set", through="Like")
+
+    @property # 데코레이터에 property를 붙이면 get method를 선언.
+    def like_count(self): # 얘는 게시글에 달린 좋아요의 갯수를 세는 함수다. self는 모델의 객체를 의미한다. Post가 아니라 post에 달린 좋아요 갯수를 셀 거니까.
+        '''
+        아쒸발이해가잘안된당
+        일단 Like 모델링 (Post 객체와 User 객체를 한 쌍으로 연결 / post와 user를 Like 모델에 저장) ->
+        지금 이 Post 모델에 like_user_set 속성 추가 (User 모델과 M:N 관계 형성 / Like 모델의 데이터를 Post와 User의 M:N 관계와 연결) ->
+        데코레이터로 지금 이 like_count 함수 생성 (post에 좋아요 표시를 한 user 데이터 갯수를 카운트 / 결국 Like 모델 객체의 갯수를 카운트)   
+        '''
+        return self.like_user_set.count() 
 
 
 class Comment(models.Model):
@@ -54,3 +70,13 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+
+class Like(models.Model): # User와 Post 두 모델의 데이터를 저장할 중간(intermediate) 모델
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # user(1) : Like(N) 한 명의 사용자가 여러 게시글에 좋아요를 달 수 있다.
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) # post(1) : Like(N) 한 게시글에 여러 개의 좋아요가 달릴 수 있다.
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta: # 모델 안에 메타데이터를 추가할 수 있다. 모델 단위에서 설정할 수 있는 옵션이라 생각하면 된다. 모델 필드 아래쪽에 반드시 한 줄 공백을 줘야 한다.
+        unique_together = (('user', 'post')) #unique_together는 '함께 유일해야 하는 필드의 쌍'을 의미한다. A 유저가 B 게시글에 누른 좋아요가 중복될 수는 없으니까.
