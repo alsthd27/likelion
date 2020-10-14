ce9e95449686d5e1b89ca8f0ca90f108a458aace
Yomin Song <alsthd27@naver.com>
2020-10-14 20:47:01 +0900

    조회수 기능

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,8 +1,9 @@
 # render는 '템플릿을 불러오는 것', redirect는 'URL로 이동하는 것(해당 URL의 views 함수를 실행하는 것)'.
 from django.shortcuts import render, redirect, get_object_or_404 # "django.shortcuts 패키지에 있는 render 함수 등을 사용하로독 하겄따잉!"
-from .models import Post
+from .models import *
 
 # 함수와 함수 사이는 두 줄 간격이 좋다!
+# 프로그래밍에서 =는 같다는 뜻이 아니라 대입한다는 뜻!!!!! 같다는 의미로 사용할 땐 ==를 쓴다.
 
 def main(request): # "이걸 main이라는 함수로 명명하겠다! request를 인자로 받는다!"
     all_posts = Post.objects.all() # "Post 클래스의 모든 항목들을 all_posts(변수)라고 부르겠다!"
@@ -26,6 +27,21 @@ def create(request):
 
 def show(request, post_id): # 특정 글을 가져오려면 해당 글의 고유 id를 알아야겠지??
     post = Post.objects.get(pk=post_id) # pk = primary key (id와 비슷한 뜻)
+    ''' 요렇게 쓴 걸
+    incresed_view_count = post.view_count + 1 # 조회수를 하나 늘리고
+    post.view_count = incresed_view_count # 하나 늘린 조회수를 다시 원래 뷰 카운트에 대입
+    post.save() # 그리고 해당 Post 객체를 저장
+    '''
+    ''' 요렇게 줄일 수 있고
+    post.view_count = post.view_count + 1
+    post.save()
+    '''
+    ''' 요렇게까지 줄일 수 있다.
+    a += b는 a = a + b를 의미한다.
+    즉, post.view_count += 1은 post.view_count = post.view_count + 1을 의미한다.
+    '''
+    post.view_count += 1
+    post.save()
     return render(request, 'blog/show.html', {'post': post})
 
 
