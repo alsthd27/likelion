8d23d40fe2d02348be206f64191dae368380c460
Yomin Song <alsthd27@naver.com>
2020-10-14 19:49:31 +0900

    Comment 모델 주석

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -23,6 +23,34 @@ class Post(models.Model):
 class Comment(models.Model):
     content = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') # Post가 1, Comment가 N이니까, Comment에 ForeignKey 추가. (한 게시글에 여러 댓글이 달리니께~~~)
+    '''
+    Post가 1, Comment가 N이니까, Comment에 ForeignKey 추가. (한 게시글에 여러 댓글이 달리니께~~~)
+    N에서 1로 접근할 때는 바로 접근 가능! (1은 말 그대로 딱 한 개밖에 없으니까!)
+    근데 1에서 N에 접근할 때는 set을 반드시 사용해야 함! (N은 말 그대로 존나 여러 개니까!)
+    related_name은 set과 비슷하게 '역추적 할 때 사용되는 속성'!
+    ForeignKey의 related_name은 N에 접근하기 위한 이름(N을 가르키는 이름)이므로 복수형으로 지어라~!~!~!~!!
+
+    # Comment에서 Post로 접근할 때(특정 댓글이 어떤 게시글에 달려있는지 확인할 때) 터미널 예시
+    >>> from comments.models import Comment
+
+    >>> comment = Comment.objects.get(pk=특정값)
+    >>> comment.post
+
+    # Post에서 Comment로 접근할 때(특정 게시글에 달린 댓글들을 확인할 때) 터미널 예시
+    >>> from posts.models import Post
+
+    >>> post = Post.objects.get(pk=특정값)
+    >>> post.comment_set
+    >>> post.comment_set.add()
+    >>> post.comment_set.count()
+
+    # 근데 만약 related_name을 설정해줬다면?
+    >>> from posts.models import Post
+
+    >>> post = Post.objects.get(id=특정값)
+    >>> post.comments
+    하면 set을 쓰지 않고도 post.comments_set의 효과를 볼 수 있음.
+    '''
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
